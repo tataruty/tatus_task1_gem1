69e721d04916c86d71666812fc6d73aa9f679ed7
Tetiana Usova <tataruty@outlook.com>
2022-07-20 14:15:57 +0100

    commit_init

---
+++ lib/tatus_task1_gem1.rb
@@ -0,0 +1,6 @@
+require "tatus_task1_gem1/version"
+
+module TatusTask1Gem1
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/tatus_task1_gem1/version.rb
@@ -0,0 +1,3 @@
+module TatusTask1Gem1
+  VERSION = "0.1.0"
+end
